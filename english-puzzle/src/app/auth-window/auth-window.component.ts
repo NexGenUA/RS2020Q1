10bac9834986c2d37a26e8803fa5fc0e3ec31e6a
NexGenUA <mr.rinus@gmail.com>
2020-06-08 15:22:03 +0300

    feat: add stylelint

---
+++ english-puzzle/src/app/auth-window/auth-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-window',
+  templateUrl: './auth-window.component.html',
+  styleUrls: ['./auth-window.component.scss']
+})
+export class AuthWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
